54c7f7b7c81152afbcf7137ba923a258b25c8202
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-05-17 15:50:04 -0400

    added basic tests, removed other testing class

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,46 @@
+import io.github.nathannorth.riot4j.clients.RiotDevelopmentAPIClient;
+import io.github.nathannorth.riot4j.objects.ValLocale;
+import io.github.nathannorth.riot4j.objects.ValRegion;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.List;
+
+public class ClientTest {
+    @Test
+    public void testApis() {
+        final RiotDevelopmentAPIClient client = RiotDevelopmentAPIClient.builder()
+                .addKey(getKeys().get(0))
+                .build()
+                .block();
+        assert client != null;
+
+        client.getStatusUpdates(ValRegion.NORTH_AMERICA, Duration.ofSeconds(1)).blockFirst();
+
+        client.getValContent(ValRegion.NORTH_AMERICA, ValLocale.US_ENGLISH).block();
+
+        client.getActs().map(acts -> acts.getLatestActId()).flatMapMany(actId ->
+                client.getValLeaderboards(ValRegion.NORTH_AMERICA, actId, 0, 1000))
+                .blockLast();
+    }
+
+    //keys.txt is stored in root dir and holds instance-specific data (eg. bot token)
+    private static List<String> keys = null;
+    public static List<String> getKeys() {
+        if (keys == null) {
+            try {
+                keys = Files.readAllLines(Paths.get("./keys.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //filter out things we commented out in our keys
+            for (int i = keys.size() - 1; i >= 0; i--) {
+                if (keys.get(i).indexOf('#') == 0) keys.remove(i);
+            }
+        }
+        return keys;
+    }
+}
