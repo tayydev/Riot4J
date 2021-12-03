565a6440d720536bb24736445d2ed984da0502a3
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-12-03 18:05:46 -0500

    more comments for new ratelimiting

--- src/main/java/io/github/nathannorth/riot4j/queues/Bucket.java
+++ src/main/java/io/github/nathannorth/riot4j/queues/Bucket.java
@@ -7,33 +7,44 @@ import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 
+/**
+ * A bucket represents a per-method (per-endpoint) rate limit that can be hit without clogging up the global limit.
+ * Every bucket must be tied to a master bucket that prevents concurrent 429s from being created
+ */
 public class Bucket {
+    //per-bucket queue
     private final Sinks.Many<Retryable> in = Sinks.many().multicast().onBackpressureBuffer(1024, false);
 
+    //ref to master
     private final BucketManager master;
 
+    //enum for logging
     private final RateLimits limit;
 
     public Bucket(BucketManager master, RateLimits limit) {
         this.master = master;
         this.limit = limit;
 
+        //we just use tries (with concurrency of 1) until the day we die
         in.asFlux()
                 .flatMap(retryable -> useATry(retryable), 1)
                 .subscribe();
     }
 
+    //a recursive function that pushes a retryable to the master queue, then waits to see if there is issue. If master
+    // queue emits error then we wait and re-push to master
     private Mono<Boolean> useATry(Retryable retryable) {
         return master.push(retryable)
                 .onErrorResume(e -> {
-                    RateLimitedException rateLimitedException = (RateLimitedException) e; //we will only ever get this exception out of this sink
+                    RateLimitedException rateLimitedException = (RateLimitedException) e; //we will only ever get this specific exception out of this sink, hence unchecked cast
                     System.out.println("Got METHOD rate limit in bucket " + limit.name() + " from master! Delaying " + rateLimitedException.getSecs() + " seconds!");
                     return Mono.delay(Duration.ofSeconds(rateLimitedException.getSecs()))
                             .flatMap(finished -> useATry(retryable));
                 });
     }
 
-    public Mono<String> push(HttpClient.ResponseReceiver<?> input) {
+    //push something to this bucket. generates a new retryable, emits it internally, and returns a mono that will eventually fulfill request
+    Mono<String> push(HttpClient.ResponseReceiver<?> input) {
         Retryable r = new Retryable(input);
         in.emitNext(r, Sinks.EmitFailureHandler.FAIL_FAST);
         return r.getResultHandle().asMono();
--- src/main/java/io/github/nathannorth/riot4j/queues/BucketManager.java
+++ src/main/java/io/github/nathannorth/riot4j/queues/BucketManager.java
@@ -8,22 +8,30 @@ import reactor.netty.http.client.HttpClient;
 import java.time.Duration;
 import java.util.HashMap;
 
+/**
+ * A bucket manager handles all the actual web requests of its owned buckets, and provides a convenience map so outside methods can push to buckets
+ */
 public class BucketManager {
+    //map using enum for organization
     private final HashMap<RateLimits, Bucket> buckets = new HashMap<>();
     private final Sinks.Many<Retryable> in = Sinks.many().multicast().onBackpressureBuffer(1024, true);
 
     public BucketManager() {
+        //use tries till the day we die. We attach two emissions to successful tries
         in.asFlux()
                 .flatMap(retryable -> useATry(retryable)
+                                //whenever a try makes it out of the method, we emit the result to the output mono and also tell the queue that it can start sending stuff again
                                 .doOnNext(result -> retryable.getResultHandle().emitValue(result, Sinks.EmitFailureHandler.FAIL_FAST))
                                 .doOnNext(result -> retryable.getBucketHandle().emitValue(true, Sinks.EmitFailureHandler.FAIL_FAST))
                         , 1)
                 .subscribe();
     }
 
+    //recursive function to try making http requests
     private Mono<String> useATry(Retryable retryable) {
-        return retryable.getTry()
+        return retryable.getTry() //try http request
                 .onErrorResume(error -> {
+                    //catch rate limits
                     if(error instanceof RateLimitedException) {
                         RateLimitedException rateLimitedError = (RateLimitedException) error;
 
@@ -31,7 +39,7 @@ public class BucketManager {
                         if(!rateLimitedError.isMethod()) {
                             System.out.println("Hit GLOBAL rate limit! Delaying " + rateLimitedError.getSecs() + " seconds...");
                             return Mono.delay(Duration.ofSeconds(rateLimitedError.getSecs()))
-                                    .flatMap(finished -> useATry(retryable));
+                                    .flatMap(finished -> useATry(retryable)); //try again after failure
                         }
                         //method rate limit
                         else {
@@ -44,12 +52,14 @@ public class BucketManager {
                 });
     }
 
+    //when a bucket pushes to the manager we need to reset the handle it waits on, and then emit to our queue
     protected Mono<Boolean> push(Retryable r) {
-        r.setBucketHandle(Sinks.one()); //reset bucket handle
+        r.setBucketHandle(Sinks.one()); //reset bucket handle todo can sinks have race conditions / backpressure?
         in.emitNext(r, Sinks.EmitFailureHandler.FAIL_FAST);
         return r.getBucketHandle().asMono();
     }
 
+    //public method to access teh manager. Allows a user to push to a bucket given a key enum. Creates buckets as necessary.
     public Mono<String> pushToBucket(RateLimits limit, HttpClient.ResponseReceiver<?> input) {
         Bucket bucket = buckets.computeIfAbsent(limit,
                 key -> new Bucket(this, key)
