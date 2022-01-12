5e866495c4264fd397b2749a98497d3433ec9613
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2022-01-11 21:28:53 -0500

    v4 hotfixes

--- src/main/java/io/github/nathannorth/riot4j/json/valMatch/MatchlistEntryData.java
+++ src/main/java/io/github/nathannorth/riot4j/json/valMatch/MatchlistEntryData.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @JsonDeserialize(as = ImmutableMatchlistEntryData.class)
 public interface MatchlistEntryData {
     String matchId();
+    String queueId();
     long gameStartTimeMillis();
     Optional<String> teamId();
 }
