7c9b06bfcb862b44d77c526217e7e488fbc9dc1c
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2022-02-02 22:42:32 -0500

    less logging

--- src/main/java/io/github/nathannorth/riot4j/exceptions/IncompleteBuilderException.java
+++
@@ -1,7 +0,0 @@
-package io.github.nathannorth.riot4j.exceptions;
-
-public class IncompleteBuilderException extends RuntimeException {
-    public IncompleteBuilderException(String message) {
-        super(message);
-    }
-}
